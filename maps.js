ad575eee3549a0c114c9b48a894b12695ded1bf8
Deathcaller120-0 <49953301+Deathcaller120-0@users.noreply.github.com>
2019-09-13 12:39:13 -0700

    Create maps.js

---
+++ maps.js
@@ -0,0 +1,8 @@
+// Map format obj
+//Save select area
+var SL = {
+layer1:{0:{x1:3, x2:3, id:"loadDoor1"}, 1:{x1:5, x2:5, id:"loadDoor2"}}
+floor:{0:{x1:0, x2:20, id:"normGround"}}
+};
+
+var tutorial = {floor: {x1:0, x2:40, id:"normGround"}};
