b6495ee16fa0534b040f96a054b6dc4bb4ee1704
Deathcaller120-0 <49953301+Deathcaller120-0@users.noreply.github.com>
2019-09-13 12:28:27 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+var player = {posX:0, posY:0, items:{slot0:"None", slot1:"None", slot2:"None", slot3:"None", slot4:"None"}, hp:10, speed:5, passive:"None", cEffect:"None"};
+/* Player explaination
+posX/Y, easy to understand
+items, 5 slots numbered 0-4
+hp, health
+speed, 10 = 1 tile per sec
+*/
