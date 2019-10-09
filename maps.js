4a1b7c5de8474fda3a5a9b238f91bad0fb7ace1c
Deathcaller120-0 <49953301+Deathcaller120-0@users.noreply.github.com>
2019-10-09 08:32:40 -0700

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,18 +1,17 @@
 // Map format obj
 //Save select area
-var tutorialLINES = [{x0:0,y0:360,x1:80,y1:340}, {x0:80,y0:340,x1:210,y1:380}, {x0:162,y0:360,x1:900,y1:355}];
-var tutorialRECTS = [{x0:30,y0:340,W:10,H:10}];
+var tutorial = {LINES:[{x0:0,y0:360,x1:80,y1:340}, {x0:80,y0:340,x1:210,y1:380}, {x0:162,y0:360,x1:900,y1:355}], RECTS:[{x0:30,y0:340,W:10,H:10}]};
 
 /* Note for future me
 	to RENDER,
 	
 	LINES:
-	for (var i = 0; i < tutorialLINES.length; i++){
-		line(tutorialLINES[i].x0-player.posX, tutorialLINES[i].y0-player.posY, tutorialLINES[i].x1-player.posX, tutorialLINES[i].y1-player.posY);
+	for (var i = 0; i < tutorial.LINES.length; i++){
+		line(tutorial.LINES[i].x0-player.posX, tutorial.LINES[i].y0-player.posY, tutorial.LINES[i].x1-player.posX, tutorial.LINES[i].y1-player.posY);
 	}
 	
 	RECTS:
-	for (var i = 0; i < tutorialRECTS.length; i++){
-		rect(tutorialRECTS[i].x0, tutorialRECTS[i].y0, tutorialRECTS[i].W, tutorialRECTS[i].H);
+	for (var i = 0; i < tutorial.RECTS.length; i++){
+		rect(tutorial.RECTS[i].x0, tutorial.RECTS[i].y0, tutorial.RECTS[i].W, tutorial.RECTS[i].H);
 	}
 */
